ffc9d07dfb6d159f20bc369988b217c7d26b9dda
Chris Rhodes <iopred@gmail.com>
2013-10-13 10:17:16 -0700

    Implement Rect.Contains and Rect.Intersects.

--- vec2/rect.go
+++ vec2/rect.go
@@ -27,13 +27,19 @@ func (rect *Rect) ContainsPoint(p *T) bool {
 		p[1] >= rect.Min[1] && p[1] <= rect.Max[1]
 }
 
-// func (rect *Rect) Contains(other *Rect) bool {
-// 	panic("not implemented")
-// }
+func (rect *Rect) Contains(other *Rect) bool {
+	return other.Min[0] >= rect.Min[0] &&
+		other.Max[0] <= rect.Max[0] &&
+		other.Min[1] >= rect.Min[1] &&
+		other.Max[1] <= rect.Max[1]
+}
 
-// func (rect *Rect) Intersects(other *Rect) bool {
-// 	panic("not implemented")
-// }
+func (rect *Rect) Intersects(other *Rect) bool {
+	return other.Max[0] >= rect.Min[0] &&
+		other.Min[0] <= rect.Max[0] &&
+		other.Max[1] >= rect.Min[0] &&
+		other.Min[1] <= rect.Max[1]
+}
 
 // func Intersect(a, b *Rect) Rect {
 // 	panic("not implemented")
