23b38fa94a8c3582e211a053e0faaa6dcc19fc96
Alex Ozer <thealexozer@gmail.com>
2014-08-26 16:25:14 -0400

    Add Array() method

--- mat4/mat4.go
+++ mat4/mat4.go
@@ -76,9 +76,13 @@ func (mat *T) Size() int {
 	return 16
 }
 
-// Slice returns the elements of the matrix as slice.
 func (mat *T) Slice() []float32 {
-	return []float32{
+	return mat.Array()[:]
+}
+
+// Array returns the elements of the matrix as slice.
+func (mat *T) Array() *[16]float32 {
+	return &[16]float32{
 		mat[0][0], mat[0][1], mat[0][2], mat[0][3],
 		mat[1][0], mat[1][1], mat[1][2], mat[1][3],
 		mat[2][0], mat[2][1], mat[2][2], mat[2][3],
