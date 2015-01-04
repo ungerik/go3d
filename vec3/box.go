a7134f577c03c22d0de947f0016bc8b818d261a2
panmari <smmuzi@gmail.com>
2015-01-04 17:37:42 +0100

    Implemented methods for center/diagonal of box.

--- vec3/box.go
+++ vec3/box.go
@@ -10,6 +10,11 @@ type Box struct {
 	Max T
 }
 
+var (
+	// MaxBox holds a box that contains the entire R3 space that can be represented as vec3
+	MaxBox = Box{MinVal, MaxVal}
+) 
+
 // ParseBox parses a Box from a string. See also String()
 func ParseBox(s string) (r Box, err error) {
 	_, err = fmt.Sscan(s, &r.Min[0], &r.Min[1], &r.Min[2], &r.Max[0], &r.Max[1], &r.Max[2])
@@ -26,6 +31,16 @@ func (box *Box) ContainsPoint(p *T) bool {
 	return p[0] >= box.Min[0] && p[0] <= box.Max[0] &&
 		p[1] >= box.Min[1] && p[1] <= box.Max[1] &&
 		p[2] >= box.Min[2] && p[2] <= box.Max[2]
+}
+
+func (box *Box) Center() T {
+	c := Add(&box.Min, &box.Max)
+	c.Scale(0.5)
+	return c
+}
+
+func (box *Box) Diagonal() T {
+	return Sub(&box.Max, &box.Min)
 }
 
 // func (box *Box) Contains(other *Box) bool {
