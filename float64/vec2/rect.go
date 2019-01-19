6f93bd73d13187530a7786e0aaf20f42ff6235b5
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-01-19 11:01:18 -0500

    Add float64/rect methods

--- float64/vec2/rect.go
+++ float64/vec2/rect.go
@@ -10,6 +10,13 @@ type Rect struct {
 	Max T
 }
 
+// NewRect creates a Rect from two points.
+func NewRect(a, b *T) (rect Rect) {
+	rect.Min = Min(a, b)
+	rect.Max = Max(a, b)
+	return rect
+}
+
 // ParseRect parses a Rect from a string. See also String()
 func ParseRect(s string) (r Rect, err error) {
 	_, err = fmt.Sscan(s, &r.Min[0], &r.Min[1], &r.Max[0], &r.Max[1])
@@ -27,9 +34,18 @@ func (rect *Rect) ContainsPoint(p *T) bool {
 		p[1] >= rect.Min[1] && p[1] <= rect.Max[1]
 }
 
-// func (rect *Rect) Contains(other *Rect) bool {
-// 	panic("not implemented")
-// }
+// Contains returns if other Rect is contained within the rectangle.
+func (rect *Rect) Contains(other *Rect) bool {
+	return rect.Min[0] <= other.Min[0] &&
+		rect.Min[1] <= other.Min[1] &&
+		rect.Max[0] >= other.Max[0] &&
+		rect.Max[1] >= other.Max[1]
+}
+
+// Area calculates the area of the rectangle.
+func (rect *Rect) Area() float64 {
+	return (rect.Max[0] - rect.Min[0]) * (rect.Max[1] - rect.Min[1])
+}
 
 // func (rect *Rect) Intersects(other *Rect) bool {
 // 	panic("not implemented")
@@ -39,6 +55,15 @@ func (rect *Rect) ContainsPoint(p *T) bool {
 // 	panic("not implemented")
 // }
 
-// func Join(a, b *Rect) Rect {
-// 	panic("not implemented")
-// }
+// Join enlarges this rectangle to contain also the given rectangle.
+func (rect *Rect) Join(other *Rect) {
+	rect.Min = Min(&rect.Min, &other.Min)
+	rect.Max = Max(&rect.Max, &other.Max)
+}
+
+// Joined returns the minimal rectangle containing both a and b.
+func Joined(a, b *Rect) (rect Rect) {
+	rect.Min = Min(&a.Min, &b.Min)
+	rect.Max = Max(&a.Max, &b.Max)
+	return rect
+}
