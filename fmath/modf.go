ea46c662c3f9039be20ceb51d135623026d35253
Hugues Bernard <hubues@gmail.com>
2021-05-18 09:00:13 +0200

    Include copy of fmath with fix for archs other than amd64

---
+++ fmath/modf.go
@@ -0,0 +1,16 @@
+// Copyright 2011 Arne Vansteenkiste. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This file provides a trivial implementation based on
+// Go's float64 math library. It may be overridden by an
+// assembly implementation when available for the platform.
+
+package fmath
+
+import "math"
+
+// float32 version of math.Fmodf
+func Mod(x, y float32) float32 {
+	return float32(math.Mod(float64(x), float64(y)))
+}
---
+++ fmath/sqrtf_decl.go
@@ -0,0 +1,8 @@
+// +build amd64
+// Copyright 2011 Arne Vansteenkiste.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package fmath
+
+func Sqrtf(x float32) float32
