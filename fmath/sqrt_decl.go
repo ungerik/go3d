f69a36fa71ff1a2a31b5c155cae77e6f14f75f9d
Erik Unger <erik@erikunger.com>
2024-05-01 11:58:52 +0200

    rename fmath.Sqrtf to Sqrt and fix build for non amd64

--- fmath/sqrt_decl.go
+++ fmath/sqrt_decl.go
@@ -1,8 +1,8 @@
-// +build amd64
+//go:build amd64
+
 // Copyright 2011 Arne Vansteenkiste.  All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
 package fmath
 
-func Sqrtf(x float32) float32
+func Sqrt(x float32) float32
