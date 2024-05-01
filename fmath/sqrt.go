7b69e2753299d6dfea98c4c066b605df2fef4d50
Erik Unger <erik@erikunger.com>
2024-05-01 11:58:52 +0200

    rename fmath.Sqrtf to Sqrt and fix build for non amd64

--- fmath/sqrt.go
+++ fmath/sqrt.go
@@ -1,3 +1,5 @@
+//go:build !amd64
+
 // Copyright 2011 Arne Vansteenkiste. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
@@ -5,12 +7,11 @@
 // This file provides a trivial implementation based on
 // Go's float64 math library. It may be overridden by an
 // assembly implementation when available for the platform.
-
 package fmath
 
 import "math"
 
-// float32 version of math.Sqrt
+// Sqrt is a float32 version of math.Sqrt
 func Sqrt(x float32) float32 {
 	return float32(math.Sqrt(float64(x)))
 }
--- fmath/sqrt_amd64.s
+++ fmath/sqrt_amd64.s
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// func Sqrtf(x float32) float32
-TEXT ·Sqrtf+0(SB),$0-16
+// func Sqrt(x float32) float32
+TEXT ·Sqrt+0(SB),$0-16
 	SQRTSS	 x+0(FP), X0
 	MOVSS   X0,r+8(FP)
 	RET
